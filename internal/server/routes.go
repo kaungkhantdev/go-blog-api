813ebceec40f2c333bdcb8b891ef6e20bfd313eb
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-11-04 17:50:55 +0700

    add: instance server

---
+++ internal/server/routes.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+
+	route := gin.Default();
+
+	route.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"data" : "hello, welcome"});
+	})
+
+	return route
+}
---
+++ internal/server/server.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type Server struct {
+	port int
+}
+
+
+func NewServer() *http.Server {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+
+	if err != nil {
+		fmt.Println("Can't parse port")
+	}
+	
+	nServer := &Server{
+		port: port,
+	}
+
+	server := &http.Server{
+		Addr: fmt.Sprintf(":%d", nServer.port),
+		Handler:      nServer.RegisterRoutes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	return server;
+}
