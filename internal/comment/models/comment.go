44f476c8548b2ca449a15dc9d03cee63be918ab5
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-11-06 13:05:23 +0700

    add: domain-driven design

---
+++ internal/comment/models/comment.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Comment struct {
+	ArticleId string    `json:"article_id"`
+	UserId    string    `json:"user_id"`
+	ParentId  string    `json:"parent_id"`
+	Content   string    `json:"content"`
+	DelFlag   string    `json:"del_flag"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
--- internal/model/otp.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Otp struct {
-	Email string `json:"email"`
-	Otp   string `json:"otp"`
-}
