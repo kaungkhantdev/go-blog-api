037b9fa077ae33f5d6fefc27e1616c81faa5b1a7
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-11-09 08:46:58 +0700

    fix: delflag type

--- internal/user/models/user.go
+++ internal/user/models/user.go
@@ -6,12 +6,12 @@ import (
 
 type User struct {
 	ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name      string    `gorm:"type:varchar(100);not null" json:"name"`
-	Email     string    `gorm:"type:varchar(100);uniqueIndex;not null" json:"email"`
-	UserName  string    `gorm:"type:varchar(50);uniqueIndex;not null" json:"username"`
+	Name      string    `gorm:"type:varchar(255);not null" json:"name"`
+	Email     string    `gorm:"type:varchar(255);uniqueIndex;not null" json:"email"`
+	UserName  string    `gorm:"type:varchar(255);uniqueIndex;not null" json:"username"`
 	AvatarUrl string    `gorm:"type:varchar(255)" json:"avatar_url"`
 	Bio       string    `gorm:"type:text" json:"bio"`
-	DelFlag   string    `gorm:"type:char(1);default:'N'" json:"del_flag"` // Assuming 'N' means not deleted
+	DelFlag   bool      `gorm:"type:tinyint(1);default:0" json:"del_flag"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 	OtpId     string    `gorm:"type:varchar(50)" json:"otp_id"`
