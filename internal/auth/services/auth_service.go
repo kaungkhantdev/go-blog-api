21213b5de6a328b9da27d297d7fecb298b9b9694
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-11-12 10:50:29 +0700

    prepare: to make auth action

--- internal/auth/services/auth_service.go
+++ internal/auth/services/auth_service.go
@@ -15,3 +15,29 @@ func NewAuthService(repo *interfaces.UserRepositoryInterface) *AuthService {
 func (auth AuthService) SignUp(data interface{}) {
 
 }
+
+func (auth AuthService) SignIn() {
+	// TODO
+	
+
+}
+
+func (auth AuthService) GetOtpViaEmail() {
+	// TODO
+	// get email
+	// check email it's ald exit or not
+	// generate otp
+	// send otp via email
+
+}
+
+func (auth AuthService) VerifyOtpViaEmail() {
+	// TODO
+	// get email
+	// get otp
+	// check otp is valid or not
+	// if valid then create user, just email
+	// add verify at in user's
+
+}
+
