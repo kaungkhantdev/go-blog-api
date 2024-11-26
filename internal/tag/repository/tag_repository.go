23f41f3673c676e4c2431fe9473c9c882252833a
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-11-26 19:28:15 +0700

    add: tag service and repo

--- internal/tag/repository/tag_repository.go
+++ internal/tag/repository/tag_repository.go
@@ -1,1 +1,35 @@
 package repository
+
+import (
+	"go-blog-api/internal/tag/interfaces"
+	"go-blog-api/internal/tag/models"
+
+	"gorm.io/gorm"
+)
+
+type TagRepository struct {
+	db *gorm.DB
+}
+
+func NewTagRepository(db *gorm.DB) interfaces.TagRepositoryInterface {
+	return &TagRepository{db: db}
+}
+
+func (repo *TagRepository) CreateTag(data *models.Tag) (models.Tag, error) {
+	if err := repo.db.Create(data).Error; err != nil {
+		return models.Tag{}, err
+	}
+	return *data, nil
+}
+
+func (repo *TagRepository) UpdateTag(id int, data *models.Tag) (models.Tag, error) {
+	var tag models.Tag
+	if err := repo.db.First(&tag, id).Error; err != nil {
+		return models.Tag{}, nil
+	}
+
+	if err := repo.db.Model(&tag).Updates(data).Error; err != nil {
+		return models.Tag{}, nil
+	}
+	return tag, nil
+}
--- internal/tag/services/tag_service.go
+++ internal/tag/services/tag_service.go
@@ -1,1 +1,22 @@
 package services
+
+import (
+	"go-blog-api/internal/tag/interfaces"
+	"go-blog-api/internal/tag/models"
+)
+
+type TagService struct {
+	repo interfaces.TagRepositoryInterface
+}
+
+func NewTagService(repo interfaces.TagRepositoryInterface) *TagService {
+	return &TagService{repo: repo}
+}
+
+func (service *TagService) CreateTag(data *models.Tag) (models.Tag, error) {
+	return service.repo.CreateTag(data)
+}
+
+func (service *TagService) UpdateTag(id int, data *models.Tag) (models.Tag, error) {
+	return service.repo.UpdateTag(id, data)
+}
