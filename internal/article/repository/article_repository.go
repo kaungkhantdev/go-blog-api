368dca58d7168ea0874d88dba9f46693100ecb1a
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-11-27 22:44:39 +0700

    add: repo for article

--- internal/article/repository/article_repository.go
+++ internal/article/repository/article_repository.go
@@ -1,1 +1,43 @@
 package repository
+
+import (
+	"go-blog-api/internal/article/interfaces"
+	"go-blog-api/internal/article/models"
+
+	"gorm.io/gorm"
+)
+
+type ArticleRepository struct {
+	db *gorm.DB
+}
+
+func NewArticleRepository(db *gorm.DB) interfaces.ArticleRepositoryInterfaces {
+	return &ArticleRepository{db: db}
+}
+
+func (repo *ArticleRepository) CreateArticle(data *models.Article) (models.Article, error) {
+	if err := repo.db.Create(data).Error; err != nil {
+		return models.Article{}, err
+	}
+	return *data, nil
+}
+
+func (repo *ArticleRepository) UpdateArticle(id int, data *models.Article) (models.Article, error) {
+	var article models.Article
+	if err := repo.db.First(&article, id).Error; err != nil {
+		return models.Article{}, err
+	}
+
+	if err := repo.db.Model(&article).Updates(data).Error; err != nil {
+		return models.Article{}, err
+	}
+	return article, nil
+}
+
+func (repo *ArticleRepository) FindOneById(id int) (models.Article, error) {
+	var article models.Article
+	if err := repo.db.First(&article, id).Error; err != nil {
+		return models.Article{}, err
+	}
+	return article, nil
+}
