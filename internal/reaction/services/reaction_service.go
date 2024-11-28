1d4f7718d87c3b9175594757d4a38378c9b4ed10
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-11-28 13:27:51 +0700

    add: all service and repository

--- internal/reaction/services/reaction_service.go
+++ internal/reaction/services/reaction_service.go
@@ -1,1 +1,22 @@
 package services
+
+import (
+	"go-blog-api/internal/reaction/interfaces"
+	"go-blog-api/internal/reaction/models"
+)
+
+type ReactionService struct {
+	repo interfaces.ReactionRepositoryInterface
+}
+
+func NewReactionService(repo interfaces.ReactionRepositoryInterface) *ReactionService {
+	return &ReactionService{repo: repo}
+}
+
+func (service *ReactionService) CreateReaction(data *models.Reaction) (models.Reaction, error) {
+	return service.repo.CreateReaction(data)
+}
+
+func (service *ReactionService) UpdateReaction(id int, data *models.Reaction) (models.Reaction, error) {
+	return service.repo.UpdateReaction(id, data)
+}
--- internal/reaction_type/interfaces/repository_interface.go
+++ internal/reaction_type/interfaces/repository_interface.go
@@ -1,1 +1,8 @@
 package interfaces
+
+import "go-blog-api/internal/reaction_type/models"
+
+type ReactionTypeRepositoryInterface interface {
+	CreateReactionType(data *models.ReactionType) (models.ReactionType, error)
+	UpdateReactionType(id int, data *models.ReactionType) (models.ReactionType, error)
+}
